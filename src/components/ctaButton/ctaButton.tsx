2fd6418668831f10af277bd13ca211da13df54fd
HaJaehyeong <hajae305@gmail.com>
2024-07-09 21:56:29 +0900

    HAJAE-3 modify CTA button component: hover design and add props

--- src/components/ctaButton/ctaButton.tsx
+++ src/components/ctaButton/ctaButton.tsx
@@ -2,12 +2,17 @@ import styles from './ctaButton.module.scss';
 
 type CtaButtonProps = {
   value: string;
+  onClick: () => void;
   type?: 'primary' | 'default' | 'ghost';
 };
 
-const CtaButton: React.FC<CtaButtonProps> = ({ value, type = 'default' }) => {
+const CtaButton: React.FC<CtaButtonProps> = ({ value, onClick, type = 'default' }) => {
   const buttonClass = `${styles.ctaButton} ${styles[type]}`;
-  return <button className={buttonClass}>{value}</button>;
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {value}
+    </button>
+  );
 };
 
 export default CtaButton;
