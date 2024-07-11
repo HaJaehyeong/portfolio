0102427c417a9ff75873e043a8e7a70d8f82f797
HaJaehyeong <hajae305@gmail.com>
2024-07-11 22:54:10 +0900

    HAJAE-3 add icon box component

---
+++ src/components/iconBox/iconBox.tsx
@@ -0,0 +1,17 @@
+import styles from './iconBox.module.scss';
+import {
+  RiReactjsFill,
+  RiHtml5Fill,
+  RiVuejsFill,
+  RiCss3Fill,
+  RiFlutterFill,
+  RiAngularjsFill,
+  RiVuejsLine,
+  RiNextjsLine,
+} from '@remixicon/react';
+
+const IconBox: React.FC = () => {
+  return <div className={styles.iconBoxWrapper}></div>;
+};
+
+export default IconBox;
