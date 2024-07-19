397039042a418c05da76dad04206c5a254765180
HaJaehyeong <hajae305@gmail.com>
2024-07-19 20:59:39 +0900

    HAJAE-3 add component: for footer

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,2 +1,5 @@
 export type TechCodesType = 'react' | 'html5' | 'vue' | 'css' | 'flutter' | 'angular' | 'variant5' | 'nextjs';
+export type SocialMediaType = 'github' | 'blog' | 'insta';
+
 export const TABS = ['hello', 'about-me', 'projects', '(-_- )/'];
+export const SOCIAL_MEDIAS = ['github', 'blog', 'insta'];
