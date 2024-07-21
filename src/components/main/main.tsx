122cf94263ce3b388191f0ab47507d4223d415b6
HaJaehyeong <hajae305@gmail.com>
2024-07-21 19:04:12 +0900

    HAJAE-4 typing effect to job

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,12 +1,35 @@
+'use client';
+import { useEffect, useState } from 'react';
 import styles from './main.module.scss';
 
 const Main: React.FC = () => {
+  const [displayedText, setDisplayedText] = useState('');
+  const [count, setCount] = useState(0);
+  const fullText = 'Front-end developer';
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (count < fullText.length) {
+        setDisplayedText((prev) => prev + fullText[count]);
+        setCount((prev) => prev + 1);
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 200);
+
+    return () => clearInterval(intervalId);
+  }, [count, fullText.length]);
+
   return (
     <div className={styles.mainWrapper}>
       <div className={styles.whoami}>
         <p className={styles.hello}>Hi all, I am</p>
         <h1 className={styles.name}>Jaehyeong Ha</h1>
-        <h2 className={styles.job}>{'> Front-end develop'}</h2>
+        <h2 className={styles.job}>
+          {'> '}
+          <span>{displayedText}</span>
+          <span className={styles.cursor}>|</span>
+        </h2>
 
         <label className={styles.comment}>// complete the game to continue</label>
         <label className={styles.comment}>// you can also see it on my Github page</label>
