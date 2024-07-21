90840005f75cd5c7a6426e02f6b19a46e182896f
HaJaehyeong <hajae305@gmail.com>
2024-07-21 10:44:49 +0900

    HAJAE-4 [WIP] modify styles: main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Header from '@/components/header/header';
 import styles from './page.module.scss';
+import Footer from '@/components/footer/footer';
 
 const Home: React.FC = () => {
-  return <></>;
+  return (
+    <div className={styles.pageWrapper}>
+      <div className={styles.mainWrapper}>
+        <Header name="Jaehyeong-Ha" />
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
