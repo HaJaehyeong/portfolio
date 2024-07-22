e67b626597a0a3c87665d0f551be3548e9e72905
HaJaehyeong <hajae305@gmail.com>
2024-07-22 19:05:18 +0900

    HAJAE-4 add game screen and snake head: snake game

--- src/components/snakeGame/snakeGame.tsx
+++ src/components/snakeGame/snakeGame.tsx
@@ -1,7 +1,29 @@
+import { useState } from 'react';
 import styles from './snakeGame.module.scss';
 
+const rows = 30;
+const cols = 51;
+
 const SnakeGame: React.FC = () => {
-  return <></>;
+  const [head, setHead] = useState<number>(615);
+
+  const getHead = (col: number, row: number): boolean => {
+    return Math.floor(head / rows) === col && head % rows === row;
+  };
+
+  const getGameScreenCells = () => {
+    return Array.from({ length: cols }, (_, colIndex) =>
+      Array.from({ length: rows }, (_, rowIndex) => (
+        <div className={`${styles.cell} ${getHead(colIndex, rowIndex) ? styles.head : ''}`}></div>
+      ))
+    );
+  };
+
+  return (
+    <div className={styles.gameBoxWrapper}>
+      <div className={styles.gameBox}>{getGameScreenCells()}</div>
+    </div>
+  );
 };
 
 export default SnakeGame;
