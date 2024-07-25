9fbc4fe099d199e30c91365746a6f371c329e084
HaJaehyeong <hajae305@gmail.com>
2024-07-25 14:57:23 +0900

    HAJAE-5 add nav click event and hover style

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,31 @@
+'use client';
 import TechnologyLogo from '@/components/technologyLogo/technologyLogo';
 import styles from './page.module.scss';
+import { useState } from 'react';
 
 const AboutMe: React.FC = () => {
+  const [activeNav, setActiveNav] = useState<'terminal' | 'user' | 'gamepad'>('terminal');
+
+  const handleNavClick = (nav: 'terminal' | 'user' | 'gamepad') => {
+    setActiveNav(nav);
+  };
+
   return (
     <div className={styles.aboutMeWrapper}>
       <div className={styles.sideNav}>
-        <TechnologyLogo icon="terminal" active />
-        <TechnologyLogo icon="user" />
-        <TechnologyLogo icon="gamepad" />
+        <TechnologyLogo
+          icon="terminal"
+          useHover
+          active={activeNav === 'terminal'}
+          onClick={() => handleNavClick('terminal')}
+        />
+        <TechnologyLogo icon="user" useHover active={activeNav === 'user'} onClick={() => handleNavClick('user')} />
+        <TechnologyLogo
+          icon="gamepad"
+          useHover
+          active={activeNav === 'gamepad'}
+          onClick={() => handleNavClick('gamepad')}
+        />
       </div>
     </div>
   );
--- src/components/technologyLogo/technologyLogo.tsx
+++ src/components/technologyLogo/technologyLogo.tsx
@@ -1,19 +1,39 @@
+'use client';
 import { Enums } from '@/enums/enums';
 import { TechCodesType } from '@/types/constants';
+import styles from './technologyLogo.module.scss';
 
 type TechnologyLogoProps = {
   icon: TechCodesType;
   size?: number;
   active?: boolean;
+  useHover?: boolean;
+  onClick?: () => void;
 };
 
-const TechnologyLogo: React.FC<TechnologyLogoProps> = ({ icon, size, active = false }) => {
+const TechnologyLogo: React.FC<TechnologyLogoProps> = ({ icon, size, active = false, useHover = false, onClick }) => {
   const targetIcon = Enums.Technology.valueOf(icon);
 
   // TODO(hajae): Loading Spinner와 같은 에러 헨들링을 할 수 있도록 수정
   if (!targetIcon) return <></>;
 
-  return <targetIcon.icon color="var(--secondary-gray)" size={size ? size : 24} opacity={active ? '1' : '0.4'} />;
+  const getClassName = () => {
+    if (active) {
+      return styles.active;
+    } else if (useHover) {
+      return styles.hover;
+    } else return '';
+  };
+
+  return (
+    <targetIcon.icon
+      className={getClassName()}
+      color="var(--secondary-gray)"
+      size={size ? size : 24}
+      cursor="pointer"
+      onClick={onClick}
+    />
+  );
 };
 
 export default TechnologyLogo;
