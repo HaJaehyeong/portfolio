049ee94885d3a9293660c511dd4ff9c54bb9c02a
HaJaehyeong <hajae305@gmail.com>
2024-07-25 14:07:51 +0900

    HAJAE-5 modify footer styles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,7 @@
+import styles from './page.module.scss';
+
 const AboutMe: React.FC = () => {
-  return <></>;
+  return <div className={styles.aboutMeWrapper}></div>;
 };
 
 export default AboutMe;
