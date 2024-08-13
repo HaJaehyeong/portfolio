c64820f24e67a49e43a943b725e4c3f8b7611bb8
HaJaehyeong <hajae305@gmail.com>
2024-08-13 17:48:58 +0900

    HAJAE-5 add component: About code showcase

--- src/components/about/code/code.tsx
+++ src/components/about/code/code.tsx
@@ -1,9 +1,11 @@
 import styles from './code.module.scss';
+import AboutCodeShowcase from './showcase/showcase';
 
 const AboutCode: React.FC = () => {
   return (
     <div className={styles.codeWrapper}>
       <div className={styles.emptyTab} />
+      <AboutCodeShowcase />
     </div>
   );
 };
---
+++ src/components/about/code/showcase/showcase.tsx
@@ -0,0 +1,7 @@
+import styles from './showcase.module.scss';
+
+const AboutCodeShowcase: React.FC = () => {
+  return <div className={styles.showcaseWrapper}></div>;
+};
+
+export default AboutCodeShowcase;
