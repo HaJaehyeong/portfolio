9f83ae66e777e36b2e2d17ad6613ab6f4a5f736d
HaJaehyeong <hajae305@gmail.com>
2024-12-09 15:48:03 +0900

    HAJAE-10 add comment: for BEM style definition

--- src/app/_components/loading-spinner/loading-spinner.tsx
+++ src/app/_components/loading-spinner/loading-spinner.tsx
@@ -1,6 +1,7 @@
 import styles from './loading-spinner.module.scss';
 
 const LoadingSpinner: React.FC = () => {
+  // NOTE(hajae): BEM 방법론으로 스타일 정의 (이후 작성된 코드부터 적용)
   return (
     <div className={styles['loading']}>
       <div className={styles['loading__loader']}></div>
