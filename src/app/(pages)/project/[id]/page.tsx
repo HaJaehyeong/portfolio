f114f85ab2d0b9475693bfc78cc1f1f91455b0c9
HaJaehyeong <hajae305@gmail.com>
2024-12-10 20:23:21 +0900

    HAJAE-10 add style: project page

--- src/app/(pages)/project/[id]/page.tsx
+++ src/app/(pages)/project/[id]/page.tsx
@@ -1,3 +1,4 @@
+import styles from './page.module.scss';
 import { notFound } from 'next/navigation';
 import fetchProjectInfo from './actions';
 
@@ -11,7 +12,23 @@ const Project: React.FC<ProjectProps> = async ({ params }) => {
     notFound();
   }
 
-  return <>project Page! {project.description}</>;
+  return (
+    <div className={styles['project']}>
+      <div className={styles['project__title-wrapper']}>
+        <div className={styles['project__title']}>
+          <span className={styles['project__title--ko']}>정부지원 R&D SM과제</span>
+          <span className={styles['project__title--en']}>
+            {' // '}
+            {project.title}
+          </span>
+        </div>
+        <div className={styles['project__sub-title']}>
+          <span>2024/04/03 ~ 2024/04/05</span>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
 };
 
 export default Project;
