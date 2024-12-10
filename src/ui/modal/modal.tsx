1d6af5b50598bbb4c37ff3f805fe001baca5a819
HaJaehyeong <hajae305@gmail.com>
2024-12-10 13:51:46 +0900

    HAJAE-10 modify modal style: wrapper

--- src/ui/modal/modal.tsx
+++ src/ui/modal/modal.tsx
@@ -1,14 +1,19 @@
+'use client';
+import { RiCloseLine } from '@remixicon/react';
 import styles from './modal.module.scss';
+import { useRouter } from 'next/navigation';
 
 type ModalProps = {
   children: React.ReactNode;
 };
 
 const Modal: React.FC<ModalProps> = ({ children }) => {
+  const router = useRouter();
+
   return (
-    <div className={styles.modalBackground}>
-      <section className={styles.modalWrapper}>
-        Close modal
+    <div className={styles['modal']}>
+      <section className={styles['modal__body']}>
+        <RiCloseLine className={styles['modal__body__close']} onClick={() => router.back()} />
         {children}
       </section>
     </div>
