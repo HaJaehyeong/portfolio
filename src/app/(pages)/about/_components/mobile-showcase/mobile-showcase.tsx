88aebd82617994dca2cc57130d554fd4b3b2dbf0
HaJaehyeong <hajae305@gmail.com>
2024-12-19 21:10:03 +0900

    HAJAE-11 modify mobile showcase component: add detaul toggle button

--- src/app/(pages)/about/_components/mobile-showcase/mobile-showcase.tsx
+++ src/app/(pages)/about/_components/mobile-showcase/mobile-showcase.tsx
@@ -1,8 +1,16 @@
 import Image from 'next/image';
 import profileImage from '/public/images/profile_image.png';
 import styles from './mobile-showcase.module.scss';
+import { RiChatSmile3Fill } from '@remixicon/react';
+import { useState } from 'react';
 
 const AboutMobileShowcase: React.FC = () => {
+  const [showDetail, setShowDetail] = useState<boolean>(false);
+
+  const handleToggleDetail = () => {
+    setShowDetail(!showDetail);
+  };
+
   return (
     <div className={styles['showcase']}>
       <span>{'// Code snippet showcase:'}</span>
@@ -18,6 +26,10 @@ const AboutMobileShowcase: React.FC = () => {
           <code className={styles['showcase__user__info-name']}>@hajae305</code>
           <div className={styles['showcase__user__info-created-at']}>Craeted 5 days ago</div>
         </div>
+        <code onClick={handleToggleDetail} className={styles['showcase__toggle']}>
+          <RiChatSmile3Fill size={14} className={styles['showcase__toggle-icon']} />
+          details
+        </code>
       </div>
     </div>
   );
