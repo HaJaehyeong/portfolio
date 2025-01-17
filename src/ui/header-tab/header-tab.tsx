94699af7831fb83977d27120128785e2eacf9d61
HaJaehyeong <hajae305@gmail.com>
2025-01-17 09:36:15 +0900

    HAJAE-13 add link: to contact

--- src/ui/header-tab/header-tab.tsx
+++ src/ui/header-tab/header-tab.tsx
@@ -17,7 +17,9 @@ const HeaderTab: React.FC<HeaderTabProps> = ({ pathname }) => {
           </Link>
         ))}
       </div>
-      <HeaderText text={'contact-me'} isTab={false} isSelected={pathname === 'contact'} />
+      <Link href={'/contact'}>
+        <HeaderText text={'contact-me'} isTab={false} isSelected={pathname === 'contact'} />
+      </Link>
     </div>
   );
 };
