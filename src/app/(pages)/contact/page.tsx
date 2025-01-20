ed498d3a553aa3faeb4bc22fbfae7110aa7aa116
HaJaehyeong <hajae305@gmail.com>
2025-01-20 14:15:46 +0900

    HAJAE-13 modify styles: contact me main page

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,12 +1,16 @@
+import styles from './page.module.scss';
 import ContactMeInfo from './_components/info/info';
 import ContactMeUrls from './_components/urls/urls';
 
 const ContactMe: React.FC = () => {
   return (
-    <>
-      <ContactMeInfo />
-      <ContactMeUrls />
-    </>
+    <div className={styles['contact-me']}>
+      <div className={styles['contact-me__infos']}>
+        <ContactMeInfo />
+        <ContactMeUrls />
+      </div>
+      <></>
+    </div>
   );
 };
 
