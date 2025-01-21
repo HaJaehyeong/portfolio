5a6f455924a09f355b317465fea77faab992aef9
HaJaehyeong <hajae305@gmail.com>
2025-01-21 19:55:59 +0900

    HAJAE-13 add mail send: in contact me form

--- src/app/(pages)/contact/_components/body-form/body-form.tsx
+++ src/app/(pages)/contact/_components/body-form/body-form.tsx
@@ -1,23 +1,51 @@
+'use client';
 import CtaButton from '@/ui/cta-button/cta-button';
 import styles from './body-form.module.scss';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const ContactMeBodyForm: React.FC = () => {
+  const form = useRef<HTMLFormElement>(null);
+  const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+  const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+  const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+
+  const sendEmail = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (form.current) {
+      if (!serviceId || !templateId || !publicKey) {
+        throw new Error('Missing EmailJS configuration');
+      }
+
+      emailjs.sendForm(serviceId, templateId, form.current, publicKey).then(
+        (response) => {
+          console.log('Email sent successfully: ', response);
+        },
+        (error) => {
+          console.error('Error sending email: ', error);
+          throw new Error('Failed to send email');
+        }
+      );
+    }
+  };
+
   return (
-    <form action="/" className={styles['form']}>
+    <form ref={form} onSubmit={sendEmail} className={styles['form']}>
       <div className={styles['form__item']}>
         <label htmlFor="">_name</label>
-        <input type="text" required />
+        <input type="text" name="from_name" required />
       </div>
       <div className={styles['form__item']}>
         <label htmlFor="">_email</label>
-        <input type="email" required />
+        <input type="email" name="from_email" required />
       </div>
       <div className={styles['form__item']}>
         <label htmlFor="">_message</label>
-        <textarea required />
+        <textarea name="message" required />
       </div>
       <div>
-        <CtaButton value="submit-message" />
+        <CtaButton value="submit-message" submit />
       </div>
     </form>
   );
--- src/ui/cta-button/cta-button.tsx
+++ src/ui/cta-button/cta-button.tsx
@@ -4,12 +4,13 @@ type CtaButtonProps = {
   value: string;
   onClick?: () => void;
   type?: 'primary' | 'default' | 'ghost';
+  submit?: boolean;
 };
 
-const CtaButton: React.FC<CtaButtonProps> = ({ value, onClick, type = 'default' }) => {
+const CtaButton: React.FC<CtaButtonProps> = ({ value, onClick, type = 'default', submit = false }) => {
   const buttonClass = `${styles.ctaButton} ${styles[type]}`;
   return (
-    <button className={buttonClass} onClick={onClick}>
+    <button className={buttonClass} onClick={onClick} type={submit ? 'submit' : 'button'}>
       {value}
     </button>
   );
