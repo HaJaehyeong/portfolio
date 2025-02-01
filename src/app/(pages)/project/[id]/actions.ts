c519d2a3054b542cfae7c999889768f25ecbe3f8
HaJaehyeong <hajae305@gmail.com>
2025-02-01 20:35:29 +0900

    release/v1.0.0.0 modify actions.ts: for static page

--- src/app/(pages)/project/[id]/actions.ts
+++ src/app/(pages)/project/[id]/actions.ts
@@ -1,22 +1,20 @@
 'use server';
-
-import { ProjectResponse } from '@/app/apis/project/[id]/route';
-
-const API_URL = process.env.APIS_URL;
+import { MOCK_PROJECT_INFO_LIST, ProjectResponse } from '@/types/constants';
 
 const fetchProjectInfo = async (projectId: string): Promise<ProjectResponse | undefined> => {
-  const res = await fetch(`${API_URL}/apis/project/${projectId}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  // NOTE(hajae): github pages 정적 페이지 배포를 위해 임시 삭제
+  // const res = await fetch(`${API_URL}/apis/project/${projectId}`, {
+  //   method: 'GET',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // });
+  // if (!res.ok) {
+  //   return undefined;
+  // }
+  // return await res.json();
 
-  if (!res.ok) {
-    return undefined;
-  }
-
-  return await res.json();
+  return MOCK_PROJECT_INFO_LIST.find((project) => project.projectId === projectId);
 };
 
 export default fetchProjectInfo;
--- src/app/(pages)/projects/actions.ts
+++ src/app/(pages)/projects/actions.ts
@@ -1,18 +1,16 @@
 'use server';
-
-import { ProjectsResponse } from '@/app/apis/projects/route';
-
-const API_URL = process.env.APIS_URL;
+import { MOCK_PROJECTS, ProjectsResponse } from '@/types/constants';
 
 const fetchProjects = async (): Promise<ProjectsResponse> => {
-  const res = await fetch(`${API_URL}/apis/projects`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  // NOTE(hajae): github pages 정적 페이지 배포를 위해 임시 삭제
+  // const res = await fetch(`${API_URL}/apis/projects`, {
+  //   method: 'GET',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // });
 
-  return await res.json();
+  return MOCK_PROJECTS;
 };
 
 export default fetchProjects;
