385fd0c0067cf032005a05832b745cf094ad72f8
HaJaehyeong <hajae305@gmail.com>
2025-02-01 20:25:50 +0900

    release/v1.0.0.0 route constants to constants.ts

--- src/app/apis/projects/route.ts
+++ src/app/apis/projects/route.ts
@@ -1,58 +1,9 @@
-import { TechnologyEnum } from '@/enums/technology.enum';
+import { MOCK_PROJECTS } from '@/types/constants';
 import { NextResponse } from 'next/server';
-
-export type ProjectsResponse = {
-  projects: Project[];
-};
-
-export type Project = {
-  proejctId: string;
-  title: string;
-  description: string;
-  mainSkill: TechnologyEnum['code'];
-  skills: TechnologyEnum['code'][];
-  imageSrc: string;
-};
 
 export async function GET(req: Request) {
   try {
-    const projects: ProjectsResponse = {
-      projects: [
-        {
-          proejctId: '1',
-          title: '_government-funded-R&D',
-          description: 'Government-funded R&D Smart Machine project',
-          mainSkill: 'nextjs',
-          skills: ['nextjs', 'react', 'typescript', 'gcp'],
-          imageSrc: '/images/project1.png',
-        },
-        {
-          proejctId: '2',
-          title: '_smart-factory',
-          description: 'Baro Factory',
-          mainSkill: 'react',
-          skills: ['nextjs', 'react', 'typescript', 'gcp'],
-          imageSrc: '/images/project2.png',
-        },
-        {
-          proejctId: '3',
-          title: '_angular-version-upgrade',
-          description: 'Next Generation Front End Project',
-          mainSkill: 'angular',
-          skills: ['angular', 'typescript', 'aws'],
-          imageSrc: '/images/project3.png',
-        },
-        {
-          proejctId: '4',
-          title: '_adding-new-feature ',
-          description: 'Selection Flow Management Improvement ',
-          mainSkill: 'angular',
-          skills: ['scala', 'java', 'angular', 'typescript', 'aws'],
-          imageSrc: '/images/project4.png',
-        },
-      ],
-    };
-    return NextResponse.json(projects, { status: 200 });
+    return NextResponse.json(MOCK_PROJECTS, { status: 200 });
   } catch (error) {
     console.error('Error:', error);
     return NextResponse.json({ error: 'An unexpected error occurred' }, { status: 500 });
